11598818232364c0c387e19174a7a494c46889a0
Tien Pham <tiendev94@gmail.com>
2019-05-07 16:57:26 +0300

    Apollo server express

---
+++ src/resolvers/user.js
@@ -0,0 +1,16 @@
+export default {
+  Query: {
+    users: (root, arg, context, info) => {
+
+    },
+    user: (root, arg, context, info) => {
+      
+    },
+  },
+  Mutation: {
+    signUp: (root, arg, context, info) => {
+
+    }
+    
+  }
+}
